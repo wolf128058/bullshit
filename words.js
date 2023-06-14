c732e74ddb8f33cfa1ea9c41c22e18eba586802c
VollmondHeuler <looperz@mailbox.org>
2023-06-14 03:20:49 +0200

    more words

--- words.js
+++ words.js
@@ -1,15 +1,26 @@
 const words = `
 Abstandsvergehen
+Brakecheck
+Fahrer/Beifahrer steigt aus
+Fahrerflucht
 Fahrzeugteile verloren
 Fußgänger laufen bei rot
 Gepiepte Schimpfwörter
 Handy am Steuer
 Ladung verloren
+Lichthupe
+Parkplatzrowdy
 Radfahrer übersehen
+Riskantes Überholmanöver Autobahn
+Riskantes Überholmanöver Landstraße
+Riskantes Überholmanöver Stadt
+Roadrage
 Rote Ampel überfahren
 Rücksichtsloser Motoradfahrer
 Sperrflächenkönig
+Sprachbefehl "Ok Garmin o.ä."
 Trunkenheitsfahrt
+übertrieben langes Hupen
 Vorfahrt missachtet: Kreisverkehr
 Vorfahrt missachtet: Kreuzung
 `;
