058f3221cb74349df7d7d60857c6a71a0d588b99
VollmondHeuler <looperz@mailbox.org>
2023-06-14 02:33:57 +0200

    add index.html, styles and js

---
+++ words.js
@@ -0,0 +1,15 @@
+const words = `
+Abstandsvergehen
+Fahrzeugteile verloren
+Fußgänger laufen bei rot
+Gepiepte Schimpfwörter
+Handy am Steuer
+Ladung verloren
+Radfahrer übersehen
+Rote Ampel überfahren
+Rücksichtsloser Motoradfahrer
+Sperrflächenkönig
+Trunkenheitsfahrt
+Vorfahrt missachtet: Kreisverkehr
+Vorfahrt missachtet: Kreuzung
+`;
