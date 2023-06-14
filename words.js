31dd780d9fc217a5de66e9e14e5e1fbfab2b23d8
VollmondHeuler <looperz@mailbox.org>
2023-06-14 23:35:12 +0200

    new words

--- words.js
+++ words.js
@@ -12,18 +12,22 @@ Handy am Steuer
 Ladung verloren
 Lichthupe
 LKW-Spurwechsel ohne Blinken
+Mittelspurschleicher
 Parkplatzrowdy
 Radfahrer ohne Licht
 Radfahrer radelt auf Zebrastreifen
 Radfahrer übersehen
+Riskanter Spurwechsel
 Riskantes Überholmanöver Autobahn
 Riskantes Überholmanöver Landstraße
 Riskantes Überholmanöver Stadt
 Roadrage
 Rote Ampel überfahren
 Rücksichtsloser Motoradfahrer
+Sekundenschlaf
 Sperrflächenkönig
 Sprachbefehl "Ok Garmin o.ä."
+Standstreifenmißbrauch
 Trunkenheitsfahrt
 übertrieben langes Hupen
 Vorfahrt missachtet: Kreisverkehr
