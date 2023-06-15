4aa13108667b59807fc908cee52eb68fa599867f
VollmondHeuler <looperz@mailbox.org>
2023-06-15 22:22:41 +0200

    just trailing spaces

--- files/content.js
+++ files/content.js
@@ -4,7 +4,7 @@ function loadWords() {
 	var bullshits = words.split("\n")
 						 .filter(word => word.length > 0)
 						 .map(word => word.trim());
-	
+
 	var randomizedWords = randomize(bullshits);
 	var counter=0;
 	while (counter<cells) {
@@ -15,7 +15,7 @@ function loadWords() {
 function randomize(words) {
 	var randomizedWords = [];
 	var tempWords = words.slice();
-	
+
 	for (var i=0; i<cells; i++) {
 		var wordCount = tempWords.length;
 		var randomIndex = Math.floor(Math.random() * wordCount);
